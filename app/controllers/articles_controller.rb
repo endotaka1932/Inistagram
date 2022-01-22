2133ab15d80ccac5f409421399c9eb81c64aaceb
遠藤貴文 <>
2022-01-22 19:08:59 +0900

    active_storage　追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,24 @@
 class ArticlesController < ApplicationController
     def index
+        @articles = Article.all
     end 
+
+    def show
+    end
+
+    def new
+        @article = current_user.articles.build
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
 end
