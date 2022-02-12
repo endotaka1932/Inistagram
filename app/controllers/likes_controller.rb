9dfaf707cb1f17aeeb091792d0205e138d10eec9
遠藤貴文 <>
2022-02-12 13:45:55 +0900

    destroy action作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,4 +4,10 @@ class LikesController < ApplicationController
         article = Article.find(params[:article_id])
         article.likes.create!(user_id: current_user.id)
     end
+
+    def destroy
+        article = Article.find(params[:article_id])
+        like = article.likes.find_by!(user_id: current_user.id)
+        like.destroy!
+    end
 end
