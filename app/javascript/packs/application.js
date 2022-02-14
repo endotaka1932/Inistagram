79c196751bd0b6af96705caa75c83b50511cdb10
遠藤貴文 <>
2022-02-14 21:37:52 +0900

    いいねの切り替え表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("channels")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from '@rails/ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 
 const handleHeartDisplay = (hasLiked) => {
@@ -37,10 +40,31 @@ document.addEventListener('DOMContentLoaded', () => {
                 handleHeartDisplay(hasLiked)
         })
 
-        // $('.inactive_heart').on('click', () => {
-        //     axios.post(`/articles/${articleId}/like`)
-        //         .then((response) => {
-        //             $('.inactive_heart').removeClass('hidden')
-        //         })
-        // })
+        $('.inactive_heart').on('click', () => {
+            axios.post(`/articles/${articleId}/like`)
+                .then((response) => {
+                    if (response === 'ok'){
+                        $('.active_heart').removeClass('hidden')
+                        $('.inactive_heart').addClass('hidden')
+                    }  
+                })
+                .catch((e) => {
+                    window.alert('error')
+                    console.log(e)
+                })
+        })
+
+        $('.active_heart').on('click', () => {
+            axios.delete(`/articles/${articleId}/like`)
+                .then((response) => {
+                    if (response === 'ok'){
+                        $('.inactive_heart').removeClass('hidden')
+                        $('.active_heart').addClass('hidden')
+                    }  
+                })
+                .catch((e) => {
+                    window.alert('error')
+                    console.log(e)
+                })
+        })        
 })
