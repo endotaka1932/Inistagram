fd46937602854dbe09575c8358ec33f5a1243bfc
遠藤貴文 <>
2022-02-18 07:15:32 +0900

    フォロワー関係構築

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,9 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationsihp', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationsihp', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def has_written(article)
     articles.exists?(id: article.id)
   end
