38d1efc8b485e4d9d7ee272dd421401cc7cf72ee
遠藤貴文 <>
2022-02-19 21:00:37 +0900

    フォロー解除機能の適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,12 +52,7 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-      if user.is_a?(User)
-        user_id = user.id
-      else
-        user_id = user
-      end
-
+    user_id = get_user_id(user)
     following_relationships.create(following_id: user_id)
   end
 
@@ -66,7 +61,8 @@ class User < ApplicationRecord
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -98,4 +94,12 @@ class User < ApplicationRecord
     end
   end
 
+  private
+    def get_user_id(user)
+      if user.is_a?(User)
+        user.id
+      else
+        user
+      end
+    end
 end
