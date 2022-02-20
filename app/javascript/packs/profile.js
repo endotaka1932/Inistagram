bed1731501cd80d5318b9878d8b803d415aed0a6
遠藤貴文 <>
2022-02-21 06:29:45 +0900

    follow-unfollow　非同期処理

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -4,6 +4,14 @@ import { csrfToken } from '@rails/ujs'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
+const relationshipDisplay = (hasFollow) => {
+    if (hasFollow) {
+        $('.unfollow').removeClass('hidden')
+    } else {
+        $('.follow').removeClass('hidden')
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const dataset = $('#profile').data()
@@ -11,10 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
     axios.get(`/accounts/${userId}/follows/${userId}`)
         .then((response) => {
             const hasFollow = response.data.hasFollow
-            if (hasFollow) {
-                $('.unfollow').removeClass('hidden')
-            } else {
-                $('.follow').removeClass('hidden')
+            relationshipDisplay((hasFollow))
+        })
+
+    $('.follow').on('click', () => {
+        axios.post(`/accounts/${userId}/follows`)
+        .then((res) => {
+            if (res === 'ok'){
+                $('.follow').addClass('.hidden')
+                $('.unfollow').removeClass('.hidden')
             }
         })
+        .catch((e) => {
+            window.alert('error')
+            console.log(e)
+        })
+    })
+
+    $('.unfollow').on('click', () => {
+        axios.post(`/accounts/${userId}/unfollows`)
+        .then((res) => {
+            if (res === 'ok'){
+                $('.unfollow').addClass('.hidden')
+                $('.follow').removeClass('.hidden')
+            }
+        })
+        .catch((e) => {
+            window.alert('error')
+            console.log(e)
+        })        
+    })
+
 })
