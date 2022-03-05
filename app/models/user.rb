a19b3e9a5673c75fcff264270750b7d507b9fc49
遠藤貴文 <>
2022-03-05 16:39:04 +0900

    UserTableにコラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer
 #  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
@@ -22,7 +27,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_one :profile, dependent: :destroy
   has_many :articles, dependent: :destroy
